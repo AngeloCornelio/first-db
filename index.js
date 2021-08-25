8a21fb82282e4dbb5880722ad4d2f88197fc8940
Angelo Cornelio <cornelio.angelo@gmail.com>
2021-08-25 13:02:36 -0700

    Connect to database

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const Sequelize = require('sequelize');
+const cred = require('./credentials.json');
 
 const app = express();
 const port = 3000;
+const sequelize = new Sequelize(
+  `postgres://${cred.username}:${cred.password}@localhost:5432/${cred.db}`,
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to database:', err);
+  });
 
 app.use(express.json());
 
